4a072dca562f2c085673879619fd22aebd0f79c7
lelderbe <fvdc@ya.ru>
2023-12-19 18:52:12 +0300

    refactor: cleanup

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -12,14 +12,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
 function sortByHeight(arr) {
-  const sorted = arr.filter((item) => item !== -1).sort((a, b) => b - a);
-  const result = arr.map((item) => (item === -1 ? item : sorted.pop()));
+    const sorted = arr.filter((item) => item !== -1).sort((a, b) => b - a);
+    const result = arr.map((item) => (item === -1 ? item : sorted.pop()));
 
-  return result;
+    return result;
 }
 
 module.exports = {
-  sortByHeight,
+    sortByHeight,
 };
 
-console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
+// console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
